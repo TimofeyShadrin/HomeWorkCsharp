22b118ad574cb362181284c7c6e689dbf01ea96c
Timofei Shadrin <tshadrin@yandex.ru>
2022-06-24 18:00:51 +0300

    Task 6 is solved

---
+++ Task6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HelloWorld 
+{ class Program 
+    { static void Main(string[] args) 
+        { 
+            //Задача 6: Напишите программу, которая на вход принимает число и 
+            //выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+            // 4 -> да
+            // -3 -> нет
+            // 7 -> нет
+
+            Console.Clear();
+
+            Console.WriteLine("Input value number: ");
+            // получаем число
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+            //Вывод результата
+            Console.WriteLine($"Your number {number} is even");
+            }
+            
+            else
+            {
+            //Вывод результата
+            Console.WriteLine($"Your number {number} is odd");
+            }
+        } 
+    } 
+} 
